90addad025fdec64e3d13a3d7b813b42995eaf56
sukvvon <sukvvon@gmail.com>
2022-08-04 12:19:36 +0900

    feat: update initial config tagline

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,7 +6,7 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: "sukv",
+  title: "sukvvon",
   tagline: "Frontend",
   url: "https://sukvvon.github.io",
   baseUrl: "/",
